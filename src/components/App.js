7b7435f3beda481df61e78a6d42e7629db3112f0
Christian Lopez <lopezchristian1610@gmail.com>
2023-06-14 20:05:37 -0400

    all useEffect fetch tests passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [dogData, setDogData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(dog => {
+                setDogData(dog.message);
+                setIsLoaded(true);
+            });
+    }, [])
+
+    if(!isLoaded) return <p>Loading...</p>
+
+    return <img src={dogData} alt="A Random Dog" />
+}
+
+export default App;
